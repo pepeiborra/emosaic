68aaac2921d00e61ad11c691d16a4e75fa2948c1
Pepe Iborra <pepeiborra@gmail.com>
2025-08-27 14:13:17 +0200

    Safari iOS full screen
    Summary:
    
    Test Plan:

--- src/mosaic/web/widget.rs
+++ src/mosaic/web/widget.rs
@@ -141,7 +141,7 @@ where
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
     <title>Mosaic Widget</title>
     <link rel="stylesheet" href="mosaic-widget.css?v={timestamp}">
     <script>
